04b08a362c83dd403e121e79c40c7df7a8f60bfc
Lucas-Souza-Barbosa-da-Silva <lucal8162@gmail.com>
2026-09-22 15:54:06 -0300

    Inicio da barra de busca

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Busca } from './busca/busca';
+import { Cesta } from './cesta/cesta';
+import { Detalhe } from './detalhe/detalhe';
+import { Esqueci } from './esqueci/esqueci';
+import { Login } from './login/login';
+import { Vitrine } from './vitrine/vitrine';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"busca", component:Busca}, {path:"Cesta", component:Cesta},
+    {path:"Detalhe", component:Detalhe}, {path:"Esqueci", component:Esqueci},
+    {path:"Login", component:Login}, {path:"Vitrine", component:Vitrine}
+
+];

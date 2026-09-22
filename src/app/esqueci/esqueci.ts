feaf1d46335b9e9017c779ac7ff6810e83ffb7f7
pedrovitor <pedrovitor@ACER-ASPIRE>
2026-09-21 22:22:07 -0300

    Criação do WebSite

---
+++ src/app/esqueci/esqueci.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-esqueci',
+  styleUrl: './esqueci.css',
+  templateUrl: './esqueci.html',
+})
+export class Esqueci {}

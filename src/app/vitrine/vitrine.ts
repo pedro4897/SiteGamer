ebf048442689d1b779cbed80db6cef5ec5084aa4
pedrovitor <pedrovitor@ACER-ASPIRE>
2026-09-23 15:30:38 -0300

    Ajuste de Pastas

--- src/app/vitrine/vitrine.ts
+++ src/app/vitrine/vitrine.ts
@@ -10,8 +10,8 @@ import { CommonModule } from '@angular/common';
 })
 export class Vitrine {
   lista: Produto[] = [
-    { id: 1, nome: 'Grand Theft Auto Sonic', descricao: 'Aventura clássica em alta velocidade.', preco: 89.90, precoPromocional: 69.90, imagem: 'Img1.png' },
-    { id: 2, nome: 'Grand Theft Auto Dragon Ball', descricao: 'Uma mistura épica de ação e nostalgia.', preco: 99.90, precoPromocional: 79.90, imagem: 'img2.png' },
+    { id: 1, nome: 'Grand Theft Auto Sonic', descricao: 'Aventura clássica em alta velocidade.', preco: 89.90, precoPromocional: 69.90, imagem: '/Img1.png' },
+    { id: 2, nome: 'Grand Theft Auto Dragon Ball', descricao: 'Uma mistura épica de ação e nostalgia.', preco: 99.90, precoPromocional: 79.90, imagem: '/img2.png' },
     { id: 3, nome: 'Lendas do Pixel', descricao: 'Uma nova jornada para os fãs dos clássicos.', preco: 79.90, precoPromocional: 59.90 },
     { id: 4, nome: 'Arena dos Campeoes', descricao: 'Entre na arena e prove seu valor.', preco: 119.90, precoPromocional: 89.90 },
     { id: 5, nome: 'Corrida Turbo', descricao: 'Velocidade, drift e muita adrenalina.', preco: 69.90, precoPromocional: 49.90 },

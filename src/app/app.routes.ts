875ca984872be6ce140bbecff7231f766c5bd6fd
pedrovitor <pedrovitor@ACER-ASPIRE>
2026-09-22 22:06:49 -0300

    Implementação do Logotipo, Navbar, barra de pesquisa e menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Login } from './login/login';
 import { Vitrine } from './vitrine/vitrine';
 
 export const routes: Routes = [
+    {path:"", redirectTo:"Vitrine", pathMatch:"full"},
     {path:"busca", component:Busca}, {path:"Cesta", component:Cesta},
     {path:"Detalhe", component:Detalhe}, {path:"Esqueci", component:Esqueci},
     {path:"Login", component:Login}, {path:"Vitrine", component:Vitrine}
